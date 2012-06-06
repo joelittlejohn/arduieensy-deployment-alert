1415dae55e2427aeb941af5e8e323d1b8a557f53
Joe Littlejohn <joelittlejohn@gmail.com>
2012-06-06 19:58:52 +0100

    Remove unnecessary function and maky jump-to! more robust

--- src/arduieensy_deployment_alert/orb.clj
+++ src/arduieensy_deployment_alert/orb.clj
@@ -1,13 +1,14 @@
 (ns arduieensy-deployment-alert.orb
-  (:import (gnu.io CommPortIdentifier SerialPort)))
+  (:import (gnu.io CommPortIdentifier SerialPort))
+  (:require [clojure.set :as set]))
 
-(def orb {:pins {:blue [12] :green [14] :red [15]
-                 :yellow [14 15] :cyan [12 14] :magenta [12 15]
-                 :all [12 14 15]}
+(def orb {:pins {:blue #{12} :green #{14} :red #{15}
+                 :yellow #{14 15} :cyan #{12 14} :magenta #{12 15}
+                 :all #{12 14 15} :none #{}}
           :pin-max 255
           :pin-min 0
           :color-ramp-interval 3
-          :output-stream (open-teensy-output-stream)
+;          :output-stream (open-teensy-output-stream)
           :current-color (ref :none)
           :throbbing? (ref false)})
 
@@ -26,26 +27,25 @@
           (byte-array [(unchecked-byte pin) (unchecked-byte value)])))
 
 (defn- jump-to! [orb color]
-  (doseq [pin (get-in orb :pins color)]
-    (write-to-pin! orb pin (orb :pin-max))))
-
-(defn- clear-color! [orb]
-  (doseq [pin (get-in orb :pins :all)]
-    (write-to-pin! orb pin (orb :pin-min))))
+  (doseq [pin (get-in orb [:pins :all])]
+    (write-to-pin! orb pin
+                   (if (some #{pin} (get-in orb [:pins color]))
+                     (orb :pin-max)
+                     (orb :pin-min)))))
 
 (defn- ramp-up! [orb color]
-  (do (clear-color! orb)
+  (do (jump-to! orb :none)
       (doseq [value (range (orb :pin-min) (orb :pin-max))
-              pin (get-in orb :pins color)]
+              pin (get-in orb [:pins color])]
         (write-to-pin! orb pin value)
         (Thread/sleep (orb :color-ramp-interval)))))
 
 (defn- ramp-down! [orb color]
-  (do (clear-color!)
-      (doseq [value (range pin-max pin-min -1)
-              pin (pins color)]
+  (do (jump-to! orb :none)
+      (doseq [value (range (orb :pin-max) (orb :pin-min) -1)
+              pin (get-in orb [:pins color])]
         (write-to-pin! pin value)
-        (Thread/sleep color-ramp-interval))))
+        (Thread/sleep (orb :color-ramp-interval)))))
 
 (defn- orb-update! [orb]
   (if (orb :throbbing?)
