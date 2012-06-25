685973ba0e8741c1468a9c8c96b07547f6c4c1c7
Joe Littlejohn <joelittlejohn@gmail.com>
2012-06-25 21:30:50 +0100

    Open stream only when orb is 'started', and rearrange for readibility

--- src/arduieensy_deployment_alert/orb.clj
+++ src/arduieensy_deployment_alert/orb.clj
@@ -2,6 +2,15 @@
   (:import (gnu.io CommPortIdentifier SerialPort))
   (:require [clojure.set :as set]))
 
+(def orb {:pins {:blue #{12} :green #{14} :red #{15}
+                 :yellow #{14 15} :cyan #{12 14} :magenta #{12 15}
+                 :all #{12 14 15} :none #{}}
+          :pin-max 255
+          :pin-min 0
+          :color-ramp-interval 3
+          :current-color (ref :none)
+          :throbbing? (ref false)})
+
 (defn- open-teensy-output-stream []
   (loop [pids (enumeration-seq (CommPortIdentifier/getPortIdentifiers))]
     (if (= CommPortIdentifier/PORT_SERIAL (.getPortType (first pids)))
@@ -10,15 +19,8 @@
             (.getOutputStream serial-port)))
       (recur (rest pids)))))
 
-(def orb {:pins {:blue #{12} :green #{14} :red #{15}
-                 :yellow #{14 15} :cyan #{12 14} :magenta #{12 15}
-                 :all #{12 14 15} :none #{}}
-          :pin-max 255
-          :pin-min 0
-          :color-ramp-interval 3
-          :output-stream (open-teensy-output-stream)
-          :current-color (ref :none)
-          :throbbing? (ref false)})
+(defn- open [orb]
+  (assoc orb :output-stream (open-teensy-output-stream)))
 
 (defn- write-to-pin! [orb pin value]
   (.write (orb :output-stream)
@@ -45,7 +47,7 @@
         (write-to-pin! orb pin value)
         (Thread/sleep (orb :color-ramp-interval)))))
  
-(defn set-color!
+(defn- set-color!
   ([orb color]
      (set-color! orb color false))
   ([orb color throbbing?]
@@ -53,7 +55,7 @@
       (ref-set (orb :current-color) color)
       (ref-set (orb :throbbing?) throbbing?))))
 
-(defn update! [orb]
+(defn- update! [orb]
   (if @(orb :throbbing?)
     (do (ramp-up! orb @(orb :current-color))
         (ramp-down! orb @(orb :current-color))
@@ -62,4 +64,8 @@
         (Thread/sleep 1000)
         (recur orb))))
 
-(update! orb)
+(defn start! []
+  "Initialize the ambient orb and allow it to begin waiting for updates"
+  (let [active-orb (open orb)]
+    (do (send-off (agent active-orb) update!)
+        active-orb)))
